0fbcb08e473d24e9d085294029b39d2bbd92f791
AdityaNikale <nikaleaditya8@gmail.com>
2025-07-23 21:12:53 +0530

    Fix: remove duplicate Nav import

--- src/components/navbar/NavbarComp.jsx
+++ src/components/navbar/NavbarComp.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import { useLogin } from "./LoginContext";
 import { toast } from 'react-toastify';
 import { Link } from "react-router-dom";
-import { Nav } from "react-bootstrap";
 
 
 const NavbarComp = () => {
